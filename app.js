e0c5f0e09acbd0c2ad8d1c9277f7bc24ea9e2b2c
Nilesh More <nilesh.more@heathrow.com>
2018-10-29 18:19:07 +0530

    code update

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
 $(function() {
+    function order_by_occurance(arr) {
+        var counts = {};
+        arr.forEach(function(val) {
+            if (!counts[val]) {
+                counts[val] = 0;
+            }
+            counts[val]++;
+        });
+    }
+
+    var last_result = [];
+    var code;
+    if (Quagga.initialized == undefined) {
+        Quagga.onDetected(function(result) {
+            var last_code = result.codeResult.code;
+            last_result.push(last_code);
+            if (last_result.length > 20) {
+                code = order_by_occurance(last_result)[0];
+                last_result = [];
+                Quagga.stop();
+
+                document.querySelector('result').innerHTML = code;
+            }
+            // var $node = null,
+            //     canvas = Quagga.canvas.dom.image;
+
+            // $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+            // $node.find("img").attr("src", canvas.toDataURL());
+            // $node.find("h4.code").html(code);
+            // $("#result_strip ul.thumbnails").prepend($node);
+        });
+    };
+
+
+
     Quagga.init({
         inputStream: {
             name: 'Live',
@@ -11,8 +46,10 @@ $(function() {
         }
     }, function(err) {
         if (err) { console.log('Error Occured...  ' + err); return; }
+        Quagga.initialized = true;
         Quagga.start();
     });
+
 
 
 });
